2da1bebe4e1e1e80a93b6729b5f843231bc8997b
Renato Souza <renatofrancisco@hotmail.com>
2024-04-14 10:50:30 -0300

    new portfolio

--- script.js
+++ script.js
@@ -1,90 +1,131 @@
-const panelsheader = document.querySelectorAll('.container-header .panel');
+// Select DOM elements
+const open_btn = document.querySelector('.open-btn');
+const close_btn = document.querySelector('.close-btn');
+const nav = document.querySelectorAll('.my-nav');
+const mainContent = document.getElementById('main-content');
+const menuItems = document.querySelectorAll('.list li a');
+const sections = document.querySelectorAll('.section');
+const projectList = document.getElementById("projectList");
 
-function removeActiveClasses(panels) {
-    panels.forEach(panel => {
-        panel.classList.remove('active');
-    });
+// Mapping between the text of the menu item and the section ID
+const idMap = {
+    'About Me': 'about',
+    'Skills': 'skills',
+    'Graduations': 'graduations',
+    'Courses': 'courses',
+    'HTML/CSS': 'html-css',
+    'Python': 'python',
+    'Javascript': 'javascript',
+    'Unity': 'unity',
+    'Contact Me': 'contact'
+};
+
+// Function to change the content
+function changeContent(e) {
+    e.preventDefault();
+    const contentId = idMap[this.textContent];
+    const contentElement = document.getElementById(contentId);
+    if (contentElement) {
+        // Hide all sections
+        sections.forEach(section => {
+            section.style.display = 'none';
+        });
+        // Show the corresponding section
+        contentElement.style.display = 'block';
+    }
 }
 
-panelsheader.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses(panelsheader);
-        panel.classList.add('active');
+// Function to handle project click
+function handleProjectClick() {
+    if (projectList.style.display === "none") {
+        projectList.style.display = "block";
+    } else {
+        projectList.style.display = "none";
+    }
 
-        var panelName = panel.getAttribute('data-panel');
-        var contentDiv = document.getElementById(panelName + '-content');
-        document.getElementById('content').innerHTML = contentDiv.innerHTML;
-        if (panelName == 'projects' || panelName == 'certifications') {
-            const panelsbody = document.querySelectorAll('.container-body .panel');
-            panelsbody[0].classList.add('active');
-            var firstPanelName = panelsbody[0].getAttribute('data-panel');
-            var firstContentDiv = document.getElementById(firstPanelName + '-body-content');
-            document.getElementById('body-content').innerHTML = firstContentDiv.innerHTML;
-            panelsbody.forEach(panel => {
-                panel.addEventListener('click', () => {
-                    removeActiveClasses(panelsbody);
-                    panel.classList.add('active');
-                    var panelName = panel.getAttribute('data-panel');
-                    var contentDiv = document.getElementById(panelName + '-body-content');
-                    document.getElementById('body-content').innerHTML = contentDiv.innerHTML;
-                    document.getElementById('body-content').scrollIntoView({ behavior: "smooth" });
-                });
-            });
-        }
-        document.getElementById('content').scrollIntoView({ behavior: "smooth" });
-    });
+    if (projectList.style.maxHeight){
+        projectList.style.maxHeight = null;
+    } else {
+        projectList.style.maxHeight = projectList.scrollHeight + "px";
+    } 
+}
+
+// Function to handle certifications click
+function handleCertificationsClick() {
+    if (certificationsList.style.display === "none") {
+        certificationsList.style.display = "block";
+    } else {
+        certificationsList.style.display = "none";
+    }
+
+    if (certificationsList.style.maxHeight){
+        certificationsList.style.maxHeight = null;
+    } else {
+        certificationsList.style.maxHeight = certificationsList.scrollHeight + "px";
+    } 
+}
+
+
+
+// Add click event to each menu item
+menuItems.forEach(item => {
+    item.addEventListener('click', changeContent);
 });
 
+// Add click event to project
+document.getElementById("projects").addEventListener("click", handleProjectClick);
 
+document.getElementById("certifications").addEventListener("click", handleCertificationsClick);
+
+
+// Function to handle menu opening
+open_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.add('visible'));
+    document.body.classList.add('menu-open'); // Add class to body
+});
+
+// Function to handle menu closing
+close_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.remove('visible'));
+    document.body.classList.remove('menu-open'); // Remove class from body
+    mainContent.classList.add('menu-closing'); // Add class when menu starts to close
+});
+
+// Function to handle end of transition
+mainContent.addEventListener('transitionend', function() {
+  mainContent.classList.remove('menu-closing'); // Remove class when transition ends
+});
+
+// Titles for typing function
 let titles = ['Hello, I\'m Renato Souza', 'Welcome to my portfolio!'];
 let i = 0;
 let titleIndex = 0;
 
+let isFirstRun = true;
+// Typing function
 function typeWriter() {
     if (i < titles[titleIndex].length) {
-        document.getElementById('title').innerHTML += titles[titleIndex].charAt(i);
+        document.getElementById('text').innerHTML += titles[titleIndex].charAt(i);
         i++;
         setTimeout(typeWriter, 100);
     } else {
-        document.getElementById('title').style.animation = 'none';
+        document.getElementById('text').style.animation = 'none';
         let delayBeforeNextTitle = titles[titleIndex] === 'Hello, I\'m Renato Souza' ? 0 : 2000;
         setTimeout(() => {
-            document.getElementById('title').innerHTML = '';
-            document.getElementById('title').style.animation = '';
+            document.getElementById('text').innerHTML = '';
+            document.getElementById('text').style.animation = '';
             i = 0;
             titleIndex = (titleIndex + 1) % titles.length;
-            let newTitle = document.getElementById('title').cloneNode(true);
-            document.getElementById('title').parentNode.replaceChild(newTitle, document.getElementById('title'));
+            let newTitle = document.getElementById('text').cloneNode(true);
+            document.getElementById('text').parentNode.replaceChild(newTitle, document.getElementById('text'));
             setTimeout(typeWriter, delayBeforeNextTitle);
         }, 3000);
     }
 }
-
-setTimeout(typeWriter, 4000);
-
-document.getElementById('content').innerHTML = document.getElementById('about-me-content').innerHTML;
-
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.loading-bg')
-
-let load = 0
-
-let int = setInterval(blurring, 30)
-
-function blurring() {
-  load++
-
-  if (load > 99) {
-    clearInterval(int)
-  }
-
-  loadText.innerText = `${load}%`
-  loadText.style.opacity = scale(load, 0, 100, 1, 0)
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-}
-
-
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+if (isFirstRun) {
+    typeWriter();
+    isFirstRun = false;
+} else {
+    setTimeout(typeWriter, 4000);
 }
 

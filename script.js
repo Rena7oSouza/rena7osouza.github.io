816ad5bca114a883429fe907a30491a576b9f099
Renato Souza <renatofrancisco@hotmail.com>
2025-07-23 16:49:14 -0300

    testing json

--- script.js
+++ script.js
@@ -165,10 +165,11 @@ typeWriter();
 // ================================
 
 async function generateCardsFromJson() {
+
     try {
         const response = await fetch('projects.json');
         const projects = await response.json();
-
+        console.log(projects);
         projects.forEach(project => {
             const section = document.getElementById(project.category);
             if (!section) {
